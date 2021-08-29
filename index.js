e8739e4a5b4cc0089f537226079a7ae89cffbe26
Agustin Fernandez <aaferna@outlook.com.ar>
2021-08-29 01:43:30 -0300

    1.2

--- index.js
+++ index.js
@@ -1,442 +1,49 @@
-const Gpio = require('onoff').Gpio;
-const c = require("loggering")
-const { DateTime } = require("luxon");
-
+const express = require('express');
 const config = require('./ports.json');
+const alarm = require('./modules/alarm');
+const path = require('path');
+const solar = require("solardb-core")
 
-const speeker = new Gpio(0, 'out');    // Pin de Speeker
-const ledRojo = new Gpio(2, 'out');   // Led Rojo
 
-speeker.writeSync(0);
-ledRojo.writeSync(0);
 
-const registreLog = (name, def) =>{
-  let dir = "./"
-  let date = DateTime.local().c;
-  c.loggering(dir,'Alarma', JSON.stringify({"date": date, "name": name, "msg": def })+",", false)
-}
+const app = express();
 
-// Activar Alarma
+let run = false
 
-  let act
-  let act2 = 0
-  
-  const activarAlarma = () => {
-    act = setInterval(() => {
-      if(act2 === 0){
-        speeker.writeSync(1)
-        ledRojo.writeSync(1);
-        act2 = 1
-      } else {
-        speeker.writeSync(0)
-        ledRojo.writeSync(0);
-        act2 = 0
-      }
-    }, 250);
+app.use(express.json())
+
+app.get('/', function(req, res) {
+
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+
+
+});
+
+app.get('/start', function(req, res) {
+
+  if(run === true){
+    res.send({ status: 201, msg: "Alarma ya iniciada"})
+  } else {
+    run = true
+    alarm.IniciarAl()
+    res.send({ status: 201, msg: "Iniciando Alarma"})
   }
 
-  const apagarAlarma = () => {
-    speeker.writeSync(0);
-    ledRojo.writeSync(0);
-    clearTimeout(act)
-    act2 = 0
+});
+
+app.get('/stop', function(req, res) {
+
+  if (run = true) {
+    alarm.ApagarAl()
+    run = false
+    res.send({ status: 201, msg: "Alarma Apagada"})
+  } else {
+    res.send({ status: 201, msg: "Alarma sin sonar"})
   }
+ 
+ 
+});
 
-
-  // No Device Active
-
-  const Aviso = () => {
-    let count = 0
-    let act = setInterval(() => {
-      if(act2 === 0){
-        speeker.writeSync(1)
-        ledRojo.writeSync(1);
-        act2 = 1
-      } else {
-        speeker.writeSync(0)
-        ledRojo.writeSync(0);
-        act2 = 0
-      }
-      if(count === 10){
-        speeker.writeSync(0);
-        clearTimeout(act)
-      }
-      count ++
-    }, 150);
-  }
-
-// No Device Active
-
-  function initAlarm() {
-    return new Promise(function(resolve, reject) {
-        let count = 0
-        let act = setInterval(() => {
-          if(act2 === 0){
-            speeker.writeSync(1)
-            ledRojo.writeSync(1);
-            act2 = 1
-          } else {
-            speeker.writeSync(0)
-            ledRojo.writeSync(0);
-            act2 = 0
-          }
-          if(count === 60){
-            speeker.writeSync(0);
-            clearTimeout(act);
-            timmerExpress();
-          }
-          count ++
-        }, 400);
-        const timmerExpress = () => {
-          let count2= 0
-          let act = setInterval(() => {
-            if(act2 === 0){
-              speeker.writeSync(1)
-              ledRojo.writeSync(1);
-              act2 = 1
-            } else {
-              speeker.writeSync(0)
-              ledRojo.writeSync(0);
-              act2 = 0
-            }
-            if(count2 === 120){
-              speeker.writeSync(0);
-              clearTimeout(act);
-              resolve(true)
-            }
-            count2 ++
-          }, 100);
-        }
-    })
-  };
-
-
-// Logica de la Alarma
-
-const IniciarAl = () =>{
-
-  let active = 0
-  for (let id in config.gpoi) {
-    if(config.gpoi[id].active === true){
-      active ++
-    }
-  }
-
-  if( active > 0){
-
-    initAlarm().then((x) => {
-
-      if(config.gpoi.p8.active === true){
-        const gpio8 = new Gpio('8', 'in', 'both'); 
-        gpio8.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p9.active === true){
-        const gpio9 = new Gpio('9', 'in', 'both'); 
-        gpio9.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p9.active === true){
-        const gpio9 = new Gpio('9', 'in', 'both'); 
-        gpio9.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p10.active === true){
-        const gpio10 = new Gpio('10', 'in', 'both'); 
-        gpio10.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p11.active === true){
-        const gpio11 = new Gpio('11', 'in', 'both'); 
-        gpio11.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p12.active === true){
-        const gpio12 = new Gpio('12', 'in', 'both'); 
-        gpio12.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p13.active === true){
-        const gpio13 = new Gpio('13', 'in', 'both'); 
-        gpio13.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p16.active === true){
-        const gpio16 = new Gpio('16', 'in', 'both'); 
-        gpio16.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p17.active === true){
-        const gpio17 = new Gpio('17', 'in', 'both'); 
-        gpio17.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p18.active === true){
-        const gpio18 = new Gpio('18', 'in', 'both'); 
-        gpio18.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p19.active === true){
-        const gpio19 = new Gpio('19', 'in', 'both'); 
-        gpio19.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p20.active === true){
-        const gpio20 = new Gpio('20', 'in', 'both'); 
-        gpio20.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p21.active === true){
-        const gpio21 = new Gpio('21', 'in', 'both'); 
-        gpio21.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p22.active === true){
-        const gpio22 = new Gpio('22', 'in', 'both'); 
-        gpio22.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p23.active === true){
-        const gpio23 = new Gpio('23', 'in', 'both'); 
-        gpio23.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p24.active === true){
-        const gpio24 = new Gpio('24', 'in', 'both'); 
-        gpio24.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p25.active === true){
-        const gpio25 = new Gpio('25', 'in', 'both'); 
-        gpio25.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p26.active === true){
-        const gpio26 = new Gpio('26', 'in', 'both'); 
-        gpio26.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else if(config.gpoi.p27.active === true){
-        const gpio27 = new Gpio('27', 'in', 'both'); 
-        gpio27.watch((err, value) => {
-          console.log(value)
-            if (err) {  registreLog(config.gpoi.p21.name, err) } 
-            else {
-              if(value === 0){ 
-                registreLog(config.gpoi.p21.name, "Sensor Activo")
-                activarAlarma() 
-              }
-              if(value === 1){ 
-                registreLog(config.gpoi.p21.name, "Sensor Apagado")
-                apagarAlarma() 
-              }
-            }
-        });
-      } else { Aviso() }
-
-    })
-
-  } else { Aviso() }
-
-}
-
-IniciarAl()
-
+app.listen("3000", () => {
+  console.log(`Sirviendo http://localhost:3000`)
+})
---
+++ modules/alarm.js
@@ -0,0 +1,220 @@
+const { DateTime } = require("luxon");
+const solar = require("solardb-core")
+
+const Gpio = require('onoff').Gpio;
+const speeker = new Gpio(0, 'out');    // Pin de Speeker
+const ledRojo = new Gpio(2, 'out');   // Led Rojo
+
+speeker.writeSync(0);
+ledRojo.writeSync(0);
+
+
+/* Registro de Actividad */
+
+  let dayActive = ""
+  let idLog = ""
+
+  const registreActivityAlarm = (gpio = null, msg = null, device = null, state = null) =>{
+
+      let r = solar.dbGetLatestFile("_ActivityAlarm_")
+      let now = DateTime.local().setZone("America/Argentina/Buenos_Aires").c
+      if(r.date != undefined && dayActive === "" && idLog === ""){
+        if(parseInt(r.date[1]) === now.day){
+          dayActive = now.day
+          idLog = r.index
+        } else {
+          dayActive = now.day
+        }
+      }
+      if(now.day === dayActive){ 
+        solar.dbUpdate({state:state, "gpio": gpio, "msg": msg, device: device, "date": now }, idLog, "_ActivityAlarm_") 
+      } else {
+        dayActive = now.day
+        idLog = solar.dbInsert({state:state, "gpio": gpio, "msg": msg, device: device, "date": now }, "_ActivityAlarm_").id
+      }
+      
+  }
+
+/* Registro de Actividad */
+
+
+/* Activar Alarma */
+
+  let actividadAlarma // proceso de la alarma
+  let stoperAlarma // proceso de la alarma
+  let sonarAlarma = false // estado de la alarma
+
+  const sonarAlarmaAct = () => {
+
+    if(sonarAlarma === false){
+
+        let estadoDeBit = 0 // Estado de Beep 1/0
+        sonarAlarma = true // Bloqueo reactivacion de alarma
+        actividadAlarma = setInterval(() => {
+            if(estadoDeBit === 0){
+            speeker.writeSync(1)
+            ledRojo.writeSync(1);
+            estadoDeBit = 1
+            } else {
+            speeker.writeSync(0)
+            ledRojo.writeSync(0);
+            estadoDeBit = 0
+            }
+        }, 250);
+        stoperAlarma = setTimeout(() => {
+            speeker.writeSync(0);
+            ledRojo.writeSync(0);
+            clearTimeout(actividadAlarma)
+            actAlarmstt = false
+            alarmaSonando = false
+        }, 3000); // Apago la alarma luego de 2 Min
+
+    } else {
+      console.log('ya esta sonando')
+    }
+
+  }
+
+  const sonarAlarmaDes = () => {
+    speeker.writeSync(0);
+    ledRojo.writeSync(0);
+    clearTimeout(actividadAlarma);
+    clearInterval(stoperAlarma);
+    sonarAlarma = false
+  }
+
+/* Activar Alarma */
+
+
+/* Aviso Sonoro de Error */
+
+  const Aviso = () => {
+      let count = 0
+      let act2 = 0
+      let act = setInterval(() => {
+          if(act2 === 0){
+          speeker.writeSync(1)
+          ledRojo.writeSync(1);
+          act2 = 1
+          } else {
+          speeker.writeSync(0)
+          ledRojo.writeSync(0);
+          act2 = 0
+          }
+          if(count === 61){
+          speeker.writeSync(0);
+          clearTimeout(act)
+          }
+          count ++
+      }, 150);
+  }
+
+/* Aviso Sonoro de Error */
+
+  function initAlarm() {
+      return new Promise(function(resolve, reject) {
+          let count = 0
+          let act2 = 0
+          let act = setInterval(() => {
+            if(act2 === 0){
+              speeker.writeSync(1)
+              ledRojo.writeSync(1);
+              act2 = 1
+            } else {
+              speeker.writeSync(0)
+              ledRojo.writeSync(0);
+              act2 = 0
+            }
+            if(count === 60){
+              speeker.writeSync(0);
+              clearTimeout(act);
+              timmerExpress();
+            }
+            count ++
+          }, 300);
+          const timmerExpress = () => {
+            let count2= 0
+            let act = setInterval(() => {
+              if(act2 === 0){
+                speeker.writeSync(1)
+                ledRojo.writeSync(1);
+                act2 = 1
+              } else {
+                speeker.writeSync(0)
+                ledRojo.writeSync(0);
+                act2 = 0
+              }
+              if(count2 === 120){
+                speeker.writeSync(0);
+                clearTimeout(act);
+                resolve(true)
+              }
+              count2 ++
+            }, 100);
+          }
+      })
+  };
+
+/* Lister GPIO */
+
+  let pinArr = []
+  const gpioListing = () => {
+      let GPIO = solar.dbGetIndex("_GPIO_")
+      for (let index = 0; index < GPIO.length; index++) {
+          let pin = solar.dbGetData(GPIO[index], "_GPIO_").pop()
+          if(pin.active === true){
+              pinArr.push(pin)
+          }
+      }
+      if(pinArr.length === 0){ Aviso() }
+  }
+
+/* Lister GPIO */
+
+/* Logica */
+
+let pin = []
+const IniciarAl = () => {
+
+    gpioListing()
+
+    if(pinArr.length != 0){ 
+      initAlarm().then( e => {
+        for (let index = 0; index < pinArr.length; index++) {
+          pin[pinArr[index].pin] = new Gpio(pinArr[index].pin, 'in', 'both'); 
+          pin[pinArr[index].pin].watch((err, value) => {
+              if (err) {  registreActivityAlarm(pinArr[index].pin, err, pinArr[index].name)} 
+              else {
+                if(value === 0){ 
+                  registreActivityAlarm(pinArr[index].pin, "Sensor Abierto", pinArr[index].name, value)
+                  sonarAlarmaAct()
+                }
+                if(value === 1){ 
+                  registreActivityAlarm(pinArr[index].pin, "Sensor Cerrado", pinArr[index].name, value)
+                  sonarAlarmaDes() 
+                }
+              }
+          });
+        }
+      })
+    }
+
+}
+
+const ApagarAl = () => {
+    sonarAlarmaDes()
+    for (let index = 0; index < pinArr.length; index++) {
+      pin[pinArr[index].pin].unwatch()
+    }
+    pinArr = []
+}
+
+/* Logica */
+
+// Exporto Modulos \\
+ 
+exports.sonarAlarmaAct = sonarAlarmaAct;
+exports.sonarAlarmaDes = sonarAlarmaDes;
+exports.Aviso = Aviso;
+exports.IniciarAl = IniciarAl;
+exports.ApagarAl = ApagarAl;
